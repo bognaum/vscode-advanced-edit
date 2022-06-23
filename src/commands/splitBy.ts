26edd546e8bc1552741aa6937b69bc100cac2cf9
Bognaum <bognaum@gmail.com>
2022-06-23 23:09:00 +0300

    It's working.

---
+++ src/commands/splitBy.ts
@@ -0,0 +1,54 @@
+import * as vsc from "vscode";
+
+export default function splitBy(tEditor: vsc.TextEditor, edit: vsc.TextEditorEdit, splitter: string) {
+	const 
+		doc  = tEditor.document,
+		opts = tEditor.options,
+		EOL  = ["", "\n", "\r\n"][doc.eol],
+		TAB  = opts.insertSpaces && typeof opts.tabSize === "number" ? 
+		" ".repeat(opts.tabSize) : "\t";
+	
+	tEditor.edit((edit) => {
+		for (let sel of tEditor.selections) {
+			const 
+				text          = doc.getText(sel),
+				firstLineText = doc.lineAt(sel.start.line).text,
+				baseIndent    = (firstLineText.match(/^\s*/) || [""])[0],
+				re = new RegExp(splitter, "g"),
+				splitters = text.match(re) || [],
+				splitted      = text.split(re);
+
+			for (const [k, v]of splitted.entries()) {}
+
+			const joiners = splitters.map(v => v + EOL + baseIndent);
+			joiners.push("");
+			const resultText = splitted
+				.map((v, i) =>  v + joiners[i])
+				.join("")
+				.split(EOL)
+				.filter(v => !v.match(/^\s*$/))
+				.join(EOL);
+
+			edit.replace(sel, resultText);
+		}
+	});
+}
+
+function getBaseIndent(text: string, offset: number) {
+	let i = offset, v = "", baseIndent = "";
+	while (v = text[i]) {
+		if (v === "\n") {
+			break;
+		}
+		i --;
+	}
+	i += 1;
+	while (v = text[i]) {
+		if (![" ", "\t"].includes(v)) {
+			break;
+		}
+		baseIndent += v;
+		i ++;
+	}
+	return baseIndent;
+}
--- src/extension.ts
+++ src/extension.ts
@@ -1,9 +1,12 @@
 import * as vsc from 'vscode';
+import splitBy from './commands/splitBy';
+
 export function activate(context: vsc.ExtensionContext) {
 	const commands = [
 		vsc.commands.registerCommand('split-by.helloWorld', () => {
 			vsc.window.showInformationMessage('Hello World from split-by!');
-		})
+		}),
+		vsc.commands.registerTextEditorCommand("splitBy.splitBy", splitBy)
 	];
 
 	context.subscriptions.push(...commands);
