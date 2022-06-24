745d635ea26d61636222c9c8e9b420e9744d9731
Bognaum <bognaum@gmail.com>
2022-06-24 14:12:12 +0300

    Added two commands and submenu.

--- src/commands/splitBy.ts
+++ src/commands/splitBy.ts
@@ -33,22 +33,3 @@ export default function splitBy(tEditor: vsc.TextEditor, edit: vsc.TextEditorEdi
 		}
 	});
 }
-
-function getBaseIndent(text: string, offset: number) {
-	let i = offset, v = "", baseIndent = "";
-	while (v = text[i]) {
-		if (v === "\n") {
-			break;
-		}
-		i --;
-	}
-	i += 1;
-	while (v = text[i]) {
-		if (![" ", "\t"].includes(v)) {
-			break;
-		}
-		baseIndent += v;
-		i ++;
-	}
-	return baseIndent;
-}
--- src/extension.ts
+++ src/extension.ts
@@ -1,12 +1,16 @@
 import * as vsc from 'vscode';
-import splitBy from './commands/splitBy';
+// import splitBy from './commands/splitBy';
+import splitByComa from './commands/splitByComa';
+import splitBySemicolon from './commands/splitBySemicolon';
 
 export function activate(context: vsc.ExtensionContext) {
 	const commands = [
 		vsc.commands.registerCommand('split-by.helloWorld', () => {
 			vsc.window.showInformationMessage('Hello World from split-by!');
 		}),
-		vsc.commands.registerTextEditorCommand("splitBy.splitBy", splitBy)
+		// vsc.commands.registerTextEditorCommand("split-by.splitBy", splitBy),
+		vsc.commands.registerTextEditorCommand("split-by.splitByComa", splitByComa),
+		vsc.commands.registerTextEditorCommand("split-by.splitBySemicolon", splitBySemicolon)
 	];
 
 	context.subscriptions.push(...commands);
